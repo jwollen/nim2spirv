7cf1aae8561d88aa1d45f17657dce3444f3c39b8
Jörg Wollenschläger <joerg.wollenschlaeger@gmail.com>
2018-09-04 23:26:13 +0900

    Update example

--- test.nim
+++ test.nim
@@ -11,14 +11,14 @@ var
   normal {.input, location: 1.}: Vector3
   texCoordVIn {.input, location: 2.}: Vector2
   texCoordVOut {.output, location: 0.}: Vector2
-  position0 {.output, builtIn: Position.}: Vector4
+  clipSpacePosition {.output, builtIn: Position.}: Vector4
 
   texCoord {.input, location: 0.}: Vector2
   color {.output, location: 0.}: Vector4
 
 proc vsMain() {.stage: Vertex.} =
-  position0 = construct[Vector4](position.xyz, 1.0'f32) * data.worldViewProjection
-  position0.y = -position0.y
+  clipSpacePosition = construct[Vector4](position.xyz, 1.0'f32) * data.worldViewProjection
+  clipSpacePosition.y = -clipSpacePosition.y
   texCoordVOut = texCoordVIn
 
 proc fsMain() {.stage: Fragment.} =
