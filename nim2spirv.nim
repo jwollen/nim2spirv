fd0a0422ed2f2e845fd4f57aa576affb3a2de6d5
Jörg Wollenschläger <joerg.wollenschlaeger@gmail.com>
2018-09-05 12:20:52 +0900

    Turn into binary nimble package

--- nim2spirv.nim
+++ nim2spirv.nim
@@ -5,7 +5,7 @@ import ../compiler/[
   modules, msgs, nimconf, options, passes, sem, lineinfos,
   platform, rod, passaux, idgen, extccomp, scriptconfig]
 
-import nim2spirv/[spirvgen, spirvSemantics]
+import src/[spirvgen, spirvSemantics]
 
 proc semanticPasses(graph: ModuleGraph) =
   registerPass(graph, verbosePass)
